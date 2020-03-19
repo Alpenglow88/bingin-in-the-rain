4053e6524853867998201424f5725a95a5c61bc6
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-03-19 17:29:39 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/films', function (req, res) {
+  res.render('films');
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});

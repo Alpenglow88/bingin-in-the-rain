6262ca436851d25f64a2eb8d791cb3bf2c624f1a
Ian Goddard <iangoddard@Mac-mini.local>
2020-03-30 14:21:18 +0100

    tidying up app.js

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const films = require('./films.json');
 
-const numberOfFilms = films.length
-
-const filmTitle1 = films[0]["film"]["title"]
-const filmOverview1 = films[0]["film"]["overview"]
-const imageUrl1 = films[0]["film"]["imageUrl"]
-const imdbScore1 = films[0]["film"]["imdbScore"]
-const trailerLink1 = films[0]["film"]["trailerLink"]
-
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/films', function (req, res) {
-  res.render('films', {filmList: films, filmCount: numberOfFilms, filmTitle1: filmTitle1, filmOverview1: filmOverview1, imageUrl1: imageUrl1, imdbScore1: imdbScore1, trailerLink1: trailerLink1});
+  res.render('films');
 });
 
 

584ead96c9cfbaa2b806d757e38ea8cfee6665d6
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-04-02 16:19:14 +0100

    fixing footer bug in 1.0.5

--- scripts/film_list_bottom.js
+++ scripts/film_list_bottom.js
@@ -4,15 +4,6 @@ const numberOfFilms = films.length
 
 const bottomChunkText = `</main>
   <footer>
-    <%- include("./partials/footer") %>
-  </footer>
-</body>
-
-</html>
-`
-
-const alphabeticalBottomChunkText = `</main>
-  <footer>
     <%- include("../partials/footer") %>
   </footer>
 </body>
@@ -24,163 +15,163 @@ fs.appendFile('views/films.ejs', bottomChunkText, function (err) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/number.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/number.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/a.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/a.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/b.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/b.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/c.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/c.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/d.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/d.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/e.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/e.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/f.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/f.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/g.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/g.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/h.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/h.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/i.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/i.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/j.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/j.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/k.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/k.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/l.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/l.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/m.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/m.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/n.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/n.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/o.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/o.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/p.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/p.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/q.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/q.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/r.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/r.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/s.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/s.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/t.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/t.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/u.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/u.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/v.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/v.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/w.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/w.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/x.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/x.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/y.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/y.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
 })
 
-fs.appendFile('views/alphabetical_views/z.ejs', alphabeticalBottomChunkText, function (
+fs.appendFile('views/alphabetical_views/z.ejs', bottomChunkText, function (
   err
 ) {
   if (err) throw err
